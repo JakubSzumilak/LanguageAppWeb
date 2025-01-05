09ec67e3e88bbd217039609bc58849c0d4370f4f
Ozzie_sergal <96013485+Ozzie-sergal@users.noreply.github.com>
2025-01-05 16:44:37 +0100

    added style and updated files.
    will be checked if everything looks and works correctly

--- languageapp/adminview/dataaccess/modifydictionary.php
+++ languageapp/adminview/dataaccess/modifydictionary.php
@@ -68,6 +68,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>database Modification</title>
+    <link rel="stylesheet" href="moddictstyle.css">
 </head>
 <body>
 <!-- 
